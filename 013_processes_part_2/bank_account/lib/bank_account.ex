8ed3fa089a59a8236069bcd5957c8ee9601dcbcc
Arjun Kathuria <arjun.kathuria8@gmail.com>
2018-03-01 01:29:16 +0530

    adds the withdraw action with test and fns

--- 013_processes_part_2/bank_account/lib/bank_account.ex
+++ 013_processes_part_2/bank_account/lib/bank_account.ex
@@ -15,4 +15,47 @@ defmodule BankAccount do
   def hello do
     :world
   end
+
+  def start do
+    await([])
+  end
+
+  def await(events) do
+    receive do
+      {:check_balance, pid} -> divulge_balance(pid, events)
+      {:deposit, amount} -> events = deposit(amount, events)
+      {:withdraw, amount} -> events = withdraw(amount, events)
+    end
+    await(events)
+  end
+
+  defp deposit(amount, events) do
+    events ++ [{:deposit, amount}]
+  end
+
+  defp withdraw(amount, events) do
+    events ++ [{:withdraw, amount}]
+  end
+    
+  defp divulge_balance(pid, events) do
+    send pid, calculate_balance(events)
+  end
+
+  defp calculate_balance(events) do
+    deposits = sum(events |> Enum.filter(fn
+      {:deposit, _} -> true
+      {_, _} -> false
+  end))
+
+    withdrawals = sum(events |> Enum.filter(fn
+    {:withdraw, _} -> true
+    {_, _ } -> false
+  end))
+
+    {:balance, deposits - withdrawals}
+  end
+
+  defp sum(events) do
+    Enum.reduce events, 0, fn({_, amount}, acc) -> amount + acc end
+  end
 end
--- 013_processes_part_2/bank_account/test/bank_account_test.exs
+++ 013_processes_part_2/bank_account/test/bank_account_test.exs
@@ -1,8 +1,32 @@
 defmodule BankAccountTest do
   use ExUnit.Case
   doctest BankAccount
-
+  
   test "greets the world" do
     assert BankAccount.hello() == :world
   end
+
+  test "starts off with a balance of 0" do
+    account = spawn_link(BankAccount, :start, [])
+    verify_balance_is 0, account
+  end
+
+  def verify_balance_is(expected_balance, pid) do
+    send pid, {:check_balance, self()}
+    assert_receive {:balance, ^expected_balance}
+  end
+
+  test "increments the balance by the amount of deposit" do
+    account = spawn_link(BankAccount, :start, [])
+    send account, {:deposit, 10}
+    verify_balance_is 10, account
+  end
+
+  test "decrements the balance by the amount of withrawal" do
+    account = spawn_link(BankAccount, :start, [])
+    send account, {:deposit, 50}
+    send account, {:withdraw, 25}
+    verify_balance_is 25, account
+  end
+  
 end
